5269b7c05ea2588b42750a693d5f46e255c7fad9
Flavia Ruffner <aruffne2@gmu.edu>
2019-07-09 02:49:51 +0000

    updating IoCsearch

--- searchIoC.py
+++ searchIoC.py
@@ -8,17 +8,20 @@ for dirName, subdirList, fileList in os.walk(basedir):
     print("Now we will search %s" % dirName)
     for file in fileList:
         print('\t%s' % file)
-        f = open(file,"r")
-        for line in f:
-            print line
-            if (line.find(IoC) != -1):
-                print( "Uh oh, you found an IoC!!")
-                i = open("IoCfound.txt","a+")
-                i.write("IoC find in %s\n" % file)
-                i.close()
-            else:
-                print("Looks good so far in %s" % file)
-
+        file_path = os.path.join(dirName,file)
+        if os.path.isfile(file_path):
+            f = open(file,"r")
+            for line in f:
+                print line
+                if (line.find(IoC) != -1):
+                    print( "Uh oh, you found an IoC!!")
+                    i = open("IoCfound.txt","a+")
+                    i.write("IoC found in %s\n" % file)
+                    i.close()
+                else:
+                    print("Looks good so far in %s" % file)
+        else:
+            print("This cannot be something you see!")
 
 
  
